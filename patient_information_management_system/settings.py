ab3d3b72d62c5cb20035b0415694ba0977baafe2
[xeroxzen] <[andilembele020@gmail.com]>
2019-12-12 10:50:50 +0200

    Debug == False

--- patient_information_management_system/settings.py
+++ patient_information_management_system/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'll!iuz+fn%79)jjfzk^ot%d)uc_4*ste-gnc)xl)qe9acj0(09'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 

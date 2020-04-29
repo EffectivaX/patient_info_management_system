893ec1c1dd38d6432db44eb2a5c8ba9699a85757
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-04-29 07:21:52 +0200

    Changes

--- patient_information_management_system/settings.py
+++ patient_information_management_system/settings.py
@@ -168,12 +168,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Additional Static files scripts
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # End here
 

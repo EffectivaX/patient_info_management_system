1c6ec74d9188a03960fba17c2ef4acc8346ac76c
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-04-29 09:01:21 +0200

    Fix Static

--- patient_information_management_system/settings.py
+++ patient_information_management_system/settings.py
@@ -171,7 +171,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
